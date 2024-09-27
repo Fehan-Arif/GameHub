b294d0f653543bc8ee22c96dc198bf6fbfd40529
Fehan Arif <fehanarif@gmail.com>
2024-09-27 11:31:03 +1000

    Add single question constructor

--- index.js
+++ index.js
@@ -43,9 +43,39 @@ app.get("/dice", (req, res) => {
     console.log(error);
   }
 });
-app.get("/trivia", (req, res) => {
+app.get("/trivia", async (req, res) => {
+  // Add constructor function
+  function Trivia(id, question, correct_answer, incorrect_answers) {
+    this.id = id;
+    this.question = question;
+    this.correct_answer = correct_answer;
+    this.incorrect_answers = incorrect_answers;
+  }
   try {
-    res.render("trivia.ejs");
+    let result = await axios.get(API_KEY); // Create a buffer from the string
+    let results = result.data.results;
+    // Constructs
+    let q1 = new Trivia(
+      1,
+      Buffer.from(results[0].question, "base64").toString("ascii"),
+      Buffer.from(results[0].correct_answer, "base64").toString("ascii"),
+      [
+        Buffer.from(results[0].incorrect_answers[0], "base64").toString(
+          "ascii",
+        ),
+        Buffer.from(results[0].incorrect_answers[1], "base64").toString(
+          "ascii",
+        ),
+        Buffer.from(results[0].incorrect_answers[2], "base64").toString(
+          "ascii",
+        ),
+      ],
+    );
+
+    // let resultsObj = new Buffer.from(results, "base64").toString("ascii");
+    // res.render("trivia.ejs", { content: results });
+    res.render("trivia.ejs", { content: results, q1: q1 });
+    console.log(q1);
   } catch (error) {
     res.status(500).send("Something went wrong.");
     console.log(error);
