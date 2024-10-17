4164c37b19927871357cc6e8ebdc5fae23a5c589
Fehan Arif <fehanarif@gmail.com>
2024-10-18 05:09:31 +1000

    Update with games.ejs as template
    Update CSS to fix form issues, rename classes
    Update games.ejs template to be properly using ejs tags for template
    functionallity. Use playGame
    Update index.js routes
    -- still working

--- index.js
+++ index.js
@@ -61,12 +61,37 @@ app.get("/:game", async (req, res) => {
   let gameType = req.params.game;
   if (gameType === "dice") {
     try {
-      res.render("games.ejs", { gameType: gameType });
+      let gameChoice = [
+        {
+          stringValue: "One",
+          intValue: 1,
+        },
+        {
+          stringValue: "Two",
+          intValue: 2,
+        },
+        {
+          stringValue: "Three",
+          intValue: 3,
+        },
+        {
+          stringValue: "Four",
+          intValue: 4,
+        },
+      ];
+      let startGame = false;
+      let choiceQuestion = "Choose Number of Players";
+      res.render("games.ejs", {
+        gameType: gameType,
+        gameChoice: gameChoice,
+        choiceQuestion: choiceQuestion,
+        startGame: startGame,
+      });
     } catch (error) {
       res.status(500).send("Something went wrong.");
       console.log(error);
     }
-  } else if(gameType === "trivia") {
+  } else if (gameType === "trivia") {
     // Add constructor function
     function Trivia(id, question, correct_answer, incorrect_answers) {
       this.id = id;
@@ -103,14 +128,73 @@ app.get("/:game", async (req, res) => {
     }
   }
 });
-app.get("/flags", (req, res) => {
-  try {
-    res.render("flags.ejs");
-  } catch (error) {
-    res.status(500).send("Something went wrong.");
-    console.log(error);
+
+app.post("/:game/play", async (req, res) => {
+  console.log(req.body);
+  let gameType = req.body.gameType;
+  let choice = req.body.choice;
+  let startGame = true;
+  let gameQuestion = "Roll Dice";
+  let choiceQuestion = "Choose Number of Players";
+  let gameChoice = [
+    {
+      stringValue: "One",
+      intValue: 1,
+    },
+    {
+      stringValue: "Two",
+      intValue: 2,
+    },
+    {
+      stringValue: "Three",
+      intValue: 3,
+    },
+    {
+      stringValue: "Four",
+      intValue: 4,
+    },
+  ];
+  let answers = [
+    { name: "roll", value: "Roll" },
+    { name: "reset", value: "Reset" },
+  ];
+  if (req.body.roll === "True") {
+    rollDie(choice);
   }
+  if (req.body.reset === "True") {
+    startGame = false;
+  }
+  // console.log(startGame);
+  res.render("games.ejs", {
+    gameType: gameType,
+    choice: choice,
+    startGame: startGame,
+    gameQuestion: gameQuestion,
+    answers: answers,
+    choiceQuestion: choiceQuestion,
+    gameChoice: gameChoice,
+  });
 });
+
+function rollDie(numPlayers) {
+  for (let index = 1; index <= numPlayers; index++) {
+    console.log(`Player ${index} got:`);
+  }
+}
+
+function resetGame() {
+  let startGame = false;
+  return startGame;
+}
+
+// app.get("/flags", (req, res) => {
+//   try {
+//     res.render("flags.ejs");
+//   } catch (error) {
+//     res.status(500).send("Something went wrong.");
+//     console.log(error);
+//   }
+// });
 // app.get("/trivia", async (req, res) => {
 //   // Add constructor function
 //   function Trivia(id, question, correct_answer, incorrect_answers) {
